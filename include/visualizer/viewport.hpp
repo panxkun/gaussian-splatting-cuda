653588318a4d069db87b765ad5e61c759733248c
panxkun <panxkun@gmail.com>
2025-06-19 22:18:27 +0800

    update

--- include/visualizer/viewport.hpp
+++ include/visualizer/viewport.hpp
@@ -1,52 +1,52 @@
 #pragma once
 
-#include <iostream>
+#include <GL/glu.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/transform.hpp>
 #include <glm/gtx/euler_angles.hpp>
-#include <GL/glu.h>
+#include <glm/gtx/transform.hpp>
+#include <iostream>
 
 class Viewport {
 
-class CameraMotion {
-public:
-    glm::vec2 prePos;
+    class CameraMotion {
+    public:
+        glm::vec2 prePos;
 
-    float zoomSpeed = 1.0f;
-    float rotateSpeed = 0.001f;
-    float translateSpeed = 0.001f;
+        float zoomSpeed = 1.0f;
+        float rotateSpeed = 0.001f;
+        float translateSpeed = 0.001f;
 
-    glm::mat3 R = glm::mat3(1.0f);
-    glm::vec3 t = glm::vec3(0.0f);
+        glm::mat3 R = glm::mat3(1.0f);
+        glm::vec3 t = glm::vec3(0.0f);
 
-    CameraMotion() = default;
+        CameraMotion() = default;
 
-    void rotate(const glm::vec2& pos) {
-        glm::vec2 delta = pos - prePos;
-        float y = -delta.x * rotateSpeed;
-        float p = +delta.y * rotateSpeed;
-        glm::mat3 Ry   = glm::mat3(glm::rotate(glm::mat4(1.0f), y, R[1]));
-        glm::mat3 Rp = glm::mat3(glm::rotate(glm::mat4(1.0f), p, R[0]));
-        R = Rp * Ry * R;
-        prePos = pos;
-    }
+        void rotate(const glm::vec2& pos) {
+            glm::vec2 delta = pos - prePos;
+            float y = -delta.x * rotateSpeed;
+            float p = +delta.y * rotateSpeed;
+            glm::mat3 Ry = glm::mat3(glm::rotate(glm::mat4(1.0f), y, R[1]));
+            glm::mat3 Rp = glm::mat3(glm::rotate(glm::mat4(1.0f), p, R[0]));
+            R = Rp * Ry * R;
+            prePos = pos;
+        }
 
-    void translate(const glm::vec2& pos) {
-        glm::vec2 delta = pos - prePos;
-        t -= (delta.x * translateSpeed) * R[0] + (delta.y * translateSpeed) * R[1];
-        prePos = pos;
-    }
+        void translate(const glm::vec2& pos) {
+            glm::vec2 delta = pos - prePos;
+            t -= (delta.x * translateSpeed) * R[0] + (delta.y * translateSpeed) * R[1];
+            prePos = pos;
+        }
 
-    void zoom(float delta) {
-        t += delta * zoomSpeed * R[2];
-    }
+        void zoom(float delta) {
+            t += delta * zoomSpeed * R[2];
+        }
 
-    void initScreenPos(const glm::vec2& pos) {
-        prePos = pos;
-    }
-};
+        void initScreenPos(const glm::vec2& pos) {
+            prePos = pos;
+        }
+    };
 
 public:
     glm::ivec2 windowSize;
