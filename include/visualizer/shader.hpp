e06b99e0e2006130d8f6e41d97b7a0e4463d4a2a
panxkun <panxkun@gmail.com>
2025-06-20 09:10:33 +0800

    remove eigen dep

--- include/visualizer/shader.hpp
+++ include/visualizer/shader.hpp
@@ -1,80 +1,92 @@
 #pragma once
 
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <string>
-#include <Eigen/Eigen>
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
+#include <glad/glad.h>
+#include <iostream>
+#include <sstream>
+#include <string>
 
-
-template <typename E> inline GLenum is_type_integral() {
+template <typename E>
+inline GLenum is_type_integral() {
     return GL_FALSE;
 }
 
-template <> inline GLenum is_type_integral<GLbyte>() {
+template <>
+inline GLenum is_type_integral<GLbyte>() {
     return GL_TRUE;
 }
 
-template <> inline GLenum is_type_integral<GLshort>() {
+template <>
+inline GLenum is_type_integral<GLshort>() {
     return GL_TRUE;
 }
 
-template <> inline GLenum is_type_integral<GLint>() {
+template <>
+inline GLenum is_type_integral<GLint>() {
     return GL_TRUE;
 }
 
-template <> inline GLenum is_type_integral<GLubyte>() {
+template <>
+inline GLenum is_type_integral<GLubyte>() {
     return GL_TRUE;
 }
 
-template <> inline GLenum is_type_integral<GLushort>() {
+template <>
+inline GLenum is_type_integral<GLushort>() {
     return GL_TRUE;
 }
 
-template <> inline GLenum is_type_integral<GLuint>() {
+template <>
+inline GLenum is_type_integral<GLuint>() {
     return GL_TRUE;
 }
 
-template <typename E> inline GLenum get_type_enum() {
+template <typename E>
+inline GLenum get_type_enum() {
     puts("Error getting type enum.");
     exit(0);
     return GL_NONE;
 }
 
-template <> inline GLenum get_type_enum<GLbyte>() {
+template <>
+inline GLenum get_type_enum<GLbyte>() {
     return GL_BYTE;
 }
 
-template <> inline GLenum get_type_enum<GLshort>() {
+template <>
+inline GLenum get_type_enum<GLshort>() {
     return GL_SHORT;
 }
 
-template <> inline GLenum get_type_enum<GLint>() {
+template <>
+inline GLenum get_type_enum<GLint>() {
     return GL_INT;
 }
 
-template <> inline GLenum get_type_enum<GLubyte>() {
+template <>
+inline GLenum get_type_enum<GLubyte>() {
     return GL_UNSIGNED_BYTE;
 }
 
-template <> inline GLenum get_type_enum<GLushort>() {
+template <>
+inline GLenum get_type_enum<GLushort>() {
     return GL_UNSIGNED_SHORT;
 }
 
-template <> inline GLenum get_type_enum<GLuint>() {
+template <>
+inline GLenum get_type_enum<GLuint>() {
     return GL_UNSIGNED_INT;
 }
 
-template <> inline GLenum get_type_enum<GLfloat>() {
+template <>
+inline GLenum get_type_enum<GLfloat>() {
     return GL_FLOAT;
 }
 
-
 class Shader {
 public:
-    Shader(const char *vshader_path, const char *fshader_path, bool create_buffer = true) {
+    Shader(const char* vshader_path, const char* fshader_path, bool create_buffer = true) {
         GLint status;
 
         std::string vshader_source = readShaderSourceFromFile(vshader_path);
@@ -89,7 +101,8 @@ public:
             if (compilation_status == GL_TRUE)
                 return;
             glGetShaderInfoLog(shader, MAX_INFO_LOG_LENGTH, &info_log_length, info_log);
-            std::cerr << "Shader compilation error:\n" << info_log << std::endl;
+            std::cerr << "Shader compilation error:\n"
+                      << info_log << std::endl;
             exit(1);
         };
 
@@ -113,7 +126,8 @@ public:
         glGetProgramiv(program, GL_LINK_STATUS, &status);
         if (status != GL_TRUE) {
             glGetProgramInfoLog(program, MAX_INFO_LOG_LENGTH, nullptr, info_log);
-            std::cerr << "Shader link error:\n" << info_log << std::endl;
+            std::cerr << "Shader link error:\n"
+                      << info_log << std::endl;
             exit(1);
         }
 
@@ -121,7 +135,6 @@ public:
             glGenBuffers(1, &index_buffer);
             glGenVertexArrays(1, &vertex_array);
         }
-
     }
 
     ~Shader() {
@@ -141,7 +154,7 @@ public:
     }
 
     void bind(bool use_buffer = true) {
-        if(use_buffer) {
+        if (use_buffer) {
             glBindVertexArray(vertex_array);
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
         }
@@ -149,7 +162,7 @@ public:
     }
 
     void unbind(bool use_buffer = true) {
-        if(use_buffer) {
+        if (use_buffer) {
             glBindVertexArray(0);
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
         }
@@ -160,50 +173,50 @@ public:
         return program;
     }
 
-    void set_uniform(const std::string &name, const size_t &value) {
+    void set_uniform(const std::string& name, const size_t& value) {
         GLint uni = uniform(name);
         glUniform1i(uni, value);
     }
 
-    void set_uniform(const std::string &name, const int &value) {
+    void set_uniform(const std::string& name, const int& value) {
         GLint uni = uniform(name);
         glUniform1i(uni, value);
     }
 
-    void set_uniform(const std::string &name, const float &value) {
+    void set_uniform(const std::string& name, const float& value) {
         GLint uni = uniform(name);
         glUniform1f(uni, value);
     }
 
-    void set_uniform(const std::string &name, const Eigen::Vector2f &vector) {
+    void set_uniform(const std::string& name, const glm::vec2& vector) {
         GLint uni = uniform(name);
-        glUniform2fv(uni, 1, vector.data());
+        glUniform2fv(uni, 1, &vector[0]);
     }
 
-    void set_uniform(const std::string &name, const Eigen::Vector3f &vector) {
+    void set_uniform(const std::string& name, const glm::vec3& vector) {
         GLint uni = uniform(name);
-        glUniform3fv(uni, 1, vector.data());
+        glUniform3fv(uni, 1, &vector[0]);
     }
 
-    void set_uniform(const std::string &name, const Eigen::Vector4f &vector) {
+    void set_uniform(const std::string& name, const glm::vec4& vector) {
         GLint uni = uniform(name);
-        glUniform4fv(uni, 1, vector.data());
+        glUniform4fv(uni, 1, &vector[0]);
     }
 
-    void set_uniform(const std::string &name, const Eigen::Matrix4f &matrix) {
+    void set_uniform(const std::string& name, const glm::mat4& matrix) {
         GLint uni = uniform(name);
-        glUniformMatrix4fv(uni, 1, GL_FALSE, matrix.data());
+        glUniformMatrix4fv(uni, 1, GL_FALSE, &matrix[0][0]);
     }
 
     // texture
-    void set_uniform(const std::string &name) {
+    void set_uniform(const std::string& name) {
         GLint uni = uniform(name);
         glUniform1i(uni, 0);
     }
 
     template <typename E, int N>
-    void set_attribute(const std::string &name,
-                    const std::vector<Eigen::Matrix<E, N, 1>> &data) {
+    void set_attribute(const std::string& name,
+                       const std::vector<glm::vec<N, E, glm::defaultp>>& data) {
         GLint attrib = attribute(name);
         if (attribute_buffers.count(attrib) == 0) {
             GLuint buffer;
@@ -212,24 +225,23 @@ public:
         }
         GLuint buffer = attribute_buffers.at(attrib);
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(E) * N * data.size(), &data[0], GL_DYNAMIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(E) * N * data.size(), data.data(), GL_DYNAMIC_DRAW);
         glEnableVertexAttribArray(attrib);
         glVertexAttribPointer(attrib, N, get_type_enum<E>(), is_type_integral<E>(), 0, nullptr);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
 
-    void set_indices(const std::vector<unsigned int> &indices) {
+    void set_indices(const std::vector<unsigned int>& indices) {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), &indices[0], GL_DYNAMIC_DRAW);
     }
-
 
     void draw(GLenum mode, GLuint start, GLuint count) {
         glDrawArrays(mode, start, count);
     }
 
     void draw_indexed(GLenum mode, GLuint start, GLuint count) {
-        glDrawElements(mode, count, GL_UNSIGNED_INT, (const void *)(start * sizeof(GLuint)));
+        glDrawElements(mode, count, GL_UNSIGNED_INT, (const void*)(start * sizeof(GLuint)));
     }
 
 private:
@@ -244,7 +256,7 @@ private:
         return buffer.str();
     }
 
-    GLint uniform(const std::string &name) {
+    GLint uniform(const std::string& name) {
         if (uniforms.count(name) == 0) {
             GLint location = glGetUniformLocation(program, name.c_str());
             if (location == -1) {
@@ -256,7 +268,7 @@ private:
         return uniforms.at(name);
     }
 
-    GLint attribute(const std::string &name) {
+    GLint attribute(const std::string& name) {
         if (attributes.count(name) == 0) {
             GLint location = glGetAttribLocation(program, name.c_str());
             if (location == -1) {
